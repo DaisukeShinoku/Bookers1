075b57232c0f56f1e66b4d25eaa343fb2ec9a5cf
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-04-18 09:52:54 +0900

    [Add]Edit Function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,18 @@ class BooksController < ApplicationController
   end
 
   def show
-  	@book = Book.find_by(params[:id])
+  	@book = Book.find(params[:id])
   end
 
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book.id)
+  end
 
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/books' => 'books#new'
   get '/' => 'homes#top'
   post '/books' => 'books#create'
-  get '/books/:id' => 'books#show'
+  get '/books/:id' => 'books#show', as:'book'
+  get '/books/:id/edit' => 'books#edit', as:'edit_book'
+  patch '/books/:id' => 'books#update', as: 'update_book'
 end
