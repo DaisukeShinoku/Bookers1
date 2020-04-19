c901c0c1161b23eec8c7b8bf3636991b5169a307
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-04-19 16:04:14 +0900

    [Fix]Create&Update Action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,11 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	if book.save
+  	@book = Book.new(book_params)
+  	if @book.save
       flash[:notice]="Book was successfully created."
   	  redirect_to book_path(book.id)
     else
-      @book = Book.new
       @books = Book.all
       render :new
     end
@@ -25,13 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	if
-      book.update(book_params)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
       flash[:notice]="Book was successfully updated."
   	  redirect_to book_path(book.id)
     else
-      @book = Book.find(params[:id])
+      # @book = Book.new(book_params)
       render :edit
     end
   end
